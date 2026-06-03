e7ee4d4b7bbb1f030f3606d4b06bcb1b7cc9ea7d
Jonas Maia <jonasgmaia2@gmail.com>
2026-06-03 14:17:10 -0300

    profile pic fix 2.0 adding cloudinary

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-
+    'cloudinary_storage',
+    'cloudinary',
     "users",
     "tweets",
 
@@ -152,3 +153,11 @@ REST_FRAMEWORK = {
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': env('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': env('CLOUDINARY_API_KEY'),
+    'API_SECRET': env('CLOUDINARY_API_SECRET'),
+}
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
